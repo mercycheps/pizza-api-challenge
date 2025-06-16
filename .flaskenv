0e7e4155f8d92b0f0ec640e9f82618b24ff0a69c
Mercy <maritimmercy8@gmail.com>
2025-06-16 10:01:57 +0300

    added url

---
+++ .flaskenv
@@ -0,0 +1,3 @@
+FLASK_APP=server/app.py
+PYTHONPATH=.
+FLASK_ENV=development
--- server/controllers/__init__.py
+++ server/controllers/__init__.py
@@ -3,6 +3,6 @@ from .restaurant_controller import restaurant_bp
 from .restaurant_pizza_controller import restaurant_pizza_bp
 
 def register_controllers(app):
-    app.register_blueprint(pizza_bp)
-    app.register_blueprint(restaurant_bp)
-    app.register_blueprint(restaurant_pizza_bp)
+    app.register_blueprint(pizza_bp, url_prefix='/pizzas')
+    app.register_blueprint(restaurant_bp,url_prefix='/restaurants')
+    app.register_blueprint(restaurant_pizza_bp,url_prefix='/restaurant_pizzas')
